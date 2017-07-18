5d3519258f2e4445f6516a6ac2d922f68215c66e
Julius <juliuscedricjomena@gmail.com>
2017-07-17 18:02:23 -0700

    second commit npm install

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('www.index');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
